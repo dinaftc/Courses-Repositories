e2fa8142e33a8233f3572694cc4e33fa17b65e1f
dinaftc <d.fettache@esi-sba.dz>
2023-07-11 21:50:57 +0100

    first

---
+++ app/about/page.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+
+
+export default function About() {
+    return (
+      <div>About us page
+        <Link href='/about/team'>team</Link>
+        
+      </div>
+       
+    )
+  }
+  
---
+++ app/about/team/page.jsx
@@ -0,0 +1,9 @@
+export const metadata = {
+    title: 'about my first next js apps team',
+  };
+export default function Team() {
+    return (
+      <div>Team page</div>
+       
+    )
+  }
